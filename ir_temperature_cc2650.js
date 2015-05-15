9bfedc88d7136160f27b1b9c1bdb61451f04c702
Burr Sutter <burrsutter@gmail.com>
2015-05-15 16:20:40 +0000

    adding cc2650

---
+++ ir_temperature_cc2650.js
@@ -0,0 +1,28 @@
+var mqtt    = require('mqtt');
+var SensorTag = require('sensortag');
+var client  = mqtt.connect('mqtt://192.168.3.2');
+
+SensorTag.discover(function(device) {
+  console.log('discovered device with UUID: ' + device['uuid']);
+  device.connect(function() {
+    console.log('connected');
+    device.discoverServicesAndCharacteristics(function() {
+      console.log('discovered services and characteristics');
+      device.enableIrTemperature(function() {
+        device.on('irTemperatureChange', function(objectTemperature, ambientTemperature) {
+          console.log('\tobject temperature is %d °C', objectTemperature.toFixed(1));
+          // console.log('\tambient temperature = %d %', ambientTemperature.toFixed(1));
+          client.publish('temp_ti_cc2650', 
+            '{"sensorid":"ti_cc2650",' +
+            ' "temp":' + objectTemperature.toFixed(1) + ',' +
+            ' "time":' + Date.now() + '}'
+            );
+        });
+
+        device.notifyIrTemperature(function() {
+          console.log('notifyIrTemperature');
+        });
+      }); // enableIrTemperature
+    }); // discoverServicesAndCharacteristics
+  }); // connect
+}); // discover
